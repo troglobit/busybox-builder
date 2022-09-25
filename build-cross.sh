1df78c7252dc86776c7af8e49f8c47949ecaae68
Joachim Wiberg <troglobit@gmail.com>
2022-09-25 04:17:37 +0200

    Use busybox_defconfig and follow best practises and upstream rec.
    Use myLinux's busybox_defconfig as base for the configuration.  However,
    not all features BusyBox use work properly with musl libc.  The latter
    is a strict POSIX C library and as such it does not have BSD extensions
    like REG_STARTEND, for instance.
    
    To build we need som extra hand waving, which the Buildroot project have
    so nicely documented in packages/pkg-kconfig.mk.  This because BusyBox
    has an older kconfig implementation and doesn't even have oldnoconfig.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- build-cross.sh
+++ build-cross.sh
@@ -22,10 +22,7 @@ make_defconfig()
 	cd busybox-"$1" || exit $?
 	exec </dev/null 2>&1 | tee BUILD.log
 
-	make defconfig >/dev/null || {
-		make defconfig # no redirects, to see all messages in log
-		exit 1
-	}
+	cp ../busybox_defconfig .config
 
 	# Set cross-compiler
 	sed 's/^CONFIG_CROSS_COMPILER_PREFIX=".*$/CONFIG_CROSS_COMPILER_PREFIX="'"$1"'"/' -i .config
@@ -37,8 +34,6 @@ make_defconfig()
 	# No idea what's the difference, but my router wants MIPS32.
 	##sed 's/^.*CONFIG_EXTRA_CFLAGS.*$/CONFIG_EXTRA_CFLAGS="-mips32"/' -i .config
 
-	sed 's/^.*CONFIG_FEATURE_MOUNT_HELPERS.*$/CONFIG_FEATURE_MOUNT_HELPERS=y/' -i .config
-
 	# These won't build because of toolchain/libc breakage:
 	##sed 's/^.*CONFIG_FEATURE_INETD_RPC.*$/# CONFIG_FEATURE_INETD_RPC is not set/' -i .config
 	# no syncfs() on armv4l, sparc
@@ -47,7 +42,27 @@ make_defconfig()
 	##sed 's/^.*CONFIG_NSENTER.*$/# CONFIG_NSENTER is not set/' -i .config
 	##sed 's/^.*CONFIG_UNSHARE.*$/# CONFIG_UNSHARE is not set/' -i .config
 
-	make oldconfig
+	# https://wiki.musl-libc.org/building-busybox.html
+	sed 's/^.*\(CONFIG_EXTRA_COMPAT\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_SELINUX\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_FEATURE_HAVE_RPC\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_WERROR\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_FEATURE_SYSTEMD\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_FEATURE_VI_REGEX_SEARCH\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_PAM\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_FEATURE_INETD_RPC\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_SELINUXENABLED\).*$/# \1 is not set/' -i .config
+	sed 's/^.*\(CONFIG_FEATURE_MOUNT_NFS\).*$/# \1 is not set/' -i .config
+
+	# Citing Buildroot: "The correct way to regenerate a .config
+	#   file is to use 'make olddefconfig'.  For historical reasons,
+	#   the target name is 'oldnoconfig' between Linux kernel
+	#   versions 2.6.36 and 3.6, and remains as an alias in later
+	#   versions.  In older versions, and in some other projects
+	#   that use kconfig, the target is not supported at all, and we
+	#   use 'yes "" | make oldconfig' as a fallback only, as this
+	#   can fail in complex cases."
+        make olddefconfig || make oldnoconfig || yes "" | make oldconfig
 	cat .config
 
 	make #V=1 || sh
